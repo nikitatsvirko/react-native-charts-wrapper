a8afe43e0ef0aa9d557be57a18153b3f11ad9dc8
Nikita <nikitatsvirko2013@outlook.com>
2018-06-20 18:53:17 +0300

    Create FloatFormatter.java

---
+++ android/src/main/java/com/github/wuxudong/rncharts/charts/FloatFormatter.java
@@ -0,0 +1,30 @@
+package com.github.wuxudong.rncharts.charts;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.DecimalFormat;
+
+public class FloatFormatter implements IAxisValueFormatter, IValueFormatter {
+
+    private DecimalFormat mFormat;
+
+    public CustomFormatter(String value) {
+        mFormat = new DecimalFormat(value);
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase yAxis) {
+        String result = value != 0.0f ? String.valueOf(value) : " ";
+        return result;
+    }
+
+    @Override
+    public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+        String result = value != 0.0f ? String.valueOf(value) : " ";
+        return result;
+    }
+}
